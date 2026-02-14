9417ba2713a4b6cb3499672d77a68fc4d9ae047c
Lazaros Papounidis <lazospap3@gmail.com>
2026-02-14 14:42:09 +0200

    34-create-grid-motion - create GridMotion for the landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { AnimatedDiv } from "@/components/motion";
+import GridMotion from "@/components/motion/GridMotion";
+import { IMAGES_URLS } from "@/constants/imagesUrls";
 import { ContainerScrollAnimation, TabsDemo } from "@/features/home/components";
 import { HomeLayout } from "@/routes/(home)/layout";
 
@@ -8,10 +11,27 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
+  /** Loop all the constants images we have for the GridMotion component. */
+  const imageItems = Object.values(IMAGES_URLS).flatMap((category) => Object.values(category));
   return (
     <HomeLayout>
-      <ContainerScrollAnimation />
-      <TabsDemo />
+      <AnimatedDiv>
+        <ContainerScrollAnimation />
+      </AnimatedDiv>
+
+      <AnimatedDiv>
+        <div className="py-10">
+          <h1 className="py-4 text-4xl font-semibold text-black dark:text-white">
+            <span className="mt-1 text-4xl leading-none font-bold md:text-[4rem]">
+              Check all the items price
+            </span>
+          </h1>
+          <GridMotion items={imageItems} gradientColor="black" />
+        </div>
+      </AnimatedDiv>
+      <AnimatedDiv>
+        <TabsDemo />
+      </AnimatedDiv>
     </HomeLayout>
   );
 }
